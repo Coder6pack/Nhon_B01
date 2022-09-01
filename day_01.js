6f0fc10032ed6a737d4d5415ec5cf34493f4d4f4
Corder6pack <hoainhonlam10@gmal.com>
2022-09-01 22:12:37 +0700

    1st commit

---
+++ day_01.js
@@ -0,0 +1,171 @@
+// Bai 1
+
+console.log("Bai 1");
+function Bai1()
+{
+   console.log("Anh Quan - \"I love you\"");
+}
+
+Bai1();
+
+
+// Bai 2
+
+console.log("Bai 2");
+
+function Bai2(a,b)
+{
+   let c=a/b;
+   console.log("Ket qua cua a/b la :"+c);
+}
+
+let a=3;
+let b=2;
+
+Bai2(a,b);
+
+// Bai 3
+
+console.log("Bai 3");
+
+function Bai3(a,b)
+{
+   let c= a + b;
+   console.log("Ket qua cua a + b la : " + c);
+}
+
+let x3=3;
+let y3=2;
+
+Bai3(x3,y3);
+
+// Bai 4
+
+console.log("Bai 4");
+
+function Bai4(a)
+{
+   let c= 5*(a-32)/9;
+
+   console.log("Ket qua cua bai 4 la : "+c);
+}
+
+let x4=32;
+
+Bai4(x4);
+
+// Bai 5
+
+console.log("Bai 5");
+
+function Bai5(a,b)
+{
+   let c = a*a+b*b;
+
+   console.log("Ket qua cua bai 5 la : " + c);
+}
+
+let x5=2;
+let y5=4;
+
+Bai5(x5,y5);
+
+// Bai 6
+
+console.log("Bai 6");
+
+function Bai6(a,b)
+{
+   let c = Math.pow(a+b,2);
+
+   console.log("Ket qua cua bai 4 la : " + c);
+}
+
+let x6=2;
+let y6=4;
+
+Bai6(x6,y6);
+
+// Bai 7
+
+console.log("Bai 7");
+
+function Bai7(a,b,c)
+{
+   let d= (a*a*b*b)/c;
+
+   console.log("Ket qua cua bai 7 la : " + d );
+}
+
+let x7=2;
+let y7=4;
+let z7=16;
+
+Bai7(x7,y7,z7);
+
+// Bai 8
+
+console.log("Bai 8");
+
+function Bai8(a,b,c,d)
+{
+   let Tb= (a+b+c+d)/4;
+
+   console.log("Ket qua cua bai 8 la : " + Tb);
+}
+
+let Toan =1;
+let Ly =2;
+let Sinh =3;
+let Anh =4;
+
+Bai8(Toan,Ly,Sinh,Anh);
+
+// Bai 9
+
+console.log("Bai 9");
+
+function Bai9(a,b)
+{
+   let c =a/b;
+
+   console.log("Ket qua cua bai 9 la : "+ typeof c);
+}
+
+let x9=3;
+let y9=0;
+
+// Bai 10
+
+console.log("Bai 10")
+
+function Bai10(a,b,c)
+{
+   let N= (-b + Math.sqrt(b*b -4*a*c))/(2*a);
+
+   console.log("Ket qua cua bai 10 la : " + N);
+}
+
+let x10=3;
+let y10=-1;
+let z10=-2
+
+Bai10(x10,y10,z10);
+
+// Bai 11
+
+console.log("Bai 11")
+
+function Bai11(a,b)
+{
+   let c = (Math.sqrt(a) + Math.abs(a))/(Math.sqrt(Math.pow(a,b)));
+
+   console.log("Ket qua cua bai 11 la : " + c);
+}
+
+let x11=4;
+let y11=2;
+
+Bai11(x11,y11);
+
+// Bai 12
